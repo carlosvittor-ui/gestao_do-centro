8d8b0826668a544b92854b555001fd3402255181
carlosvittor-ui <carlos.h.vittor@gmail.com>
2025-09-29 00:21:33 -0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -5,7 +5,7 @@ export enum Situacao {
 }
 
 export enum Funcao {
-  PaiDaCasa = 'Pai de Santp"
+  PaiDaCasa = 'Pai de Santo",
   PaiMaePequena = 'Pai ou Mãe Pequena',
   Oga = 'Ogãns',
   Curimba = 'curimba',
